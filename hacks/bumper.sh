970a4c37689e8154d49843e71bb4b6a94a88835a
Rémi REY <rrey94@gmail.com>
2020-04-16 22:59:16 +0200

    Add a script updating versions in Dockerfile

---
+++ hacks/bumper.sh
@@ -0,0 +1,67 @@
+#!/bin/bash -e
+
+DOCKERFILE_PATH="$(git rev-parse --show-toplevel)/Dockerfile"
+
+function get_terraform_latest() {
+    terraform_latest=$(curl -s https://api.github.com/repos/hashicorp/terraform/releases/latest |jq -r .tag_name)
+    terraform_latest=${terraform_latest//v}
+}
+
+function get_terragrunt_latest() {
+    terragrunt_latest=$(curl -s https://api.github.com/repos/gruntwork-io/terragrunt/releases/latest |jq -r .tag_name)
+    terragrunt_latest=${terragrunt_latest//v}
+}
+
+function get_conftest_latest() {
+    conftest_latest=$(curl -s https://api.github.com/repos/instrumenta/conftest/releases/latest |jq -r .tag_name)
+    conftest_latest=${conftest_latest//v}
+}
+
+function get_opa_latest() {
+    opa_latest=$(curl -s https://api.github.com/repos/open-policy-agent/opa/releases/latest |jq -r .tag_name)
+    opa_latest=${opa_latest//v}
+}
+
+function get_terraform_current() {
+    terraform_current=$(awk -F"=" '/ENV TERRAFORM_VERSION/{print $2}' $DOCKERFILE_PATH)
+}
+
+function get_terragrunt_current() {
+    terragrunt_current=$(awk -F"=" '/ENV TERRAGRUNT_VERSION/{print $2}' $DOCKERFILE_PATH)
+}
+
+function get_conftest_current() {
+    conftest_current=$(awk -F"=" '/ENV CONFTEST_VERSION/{print $2}' $DOCKERFILE_PATH)
+}
+
+function get_opa_current() {
+    opa_current=$(awk -F"=" '/ENV OPA_VERSION/{print $2}' $DOCKERFILE_PATH)
+}
+
+get_terraform_latest
+get_terraform_current
+
+if [ "$terraform_latest" != "$terraform_current" ]; then
+    sed -i '' "s/ENV TERRAFORM_VERSION=.*$/ENV TERRAFORM_VERSION=$terraform_latest/" $DOCKERFILE_PATH
+fi
+
+get_terragrunt_latest
+get_terragrunt_current
+
+if [ "$terragrunt_latest" != "$terragrunt_current" ]; then
+    sed -i '' "s/ENV TERRAGRUNT_VERSION=.*$/ENV TERRAGRUNT_VERSION=$terragrunt_latest/" $DOCKERFILE_PATH
+fi
+
+get_opa_latest
+get_opa_current
+
+if [ "$opa_latest" != "$opa_current" ]; then
+    sed -i '' "s/ENV OPA_VERSION=.*$/ENV OPA_VERSION=$opa_latest/" $DOCKERFILE_PATH
+fi
+
+get_conftest_latest
+get_conftest_current
+
+if [ "$conftest_latest" != "$conftest_current" ]; then
+    sed -i '' "s/ENV CONFTEST_VERSION=.*$/ENV CONFTEST_VERSION=$conftest_latest/" $DOCKERFILE_PATH
+fi
